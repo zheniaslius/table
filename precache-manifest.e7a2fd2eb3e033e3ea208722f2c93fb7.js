b872280658df2fef282eba9fb322683c8161a3f4
evsliusar <sluisarchuk@gmail.com>
2019-02-13 22:21:47 +0200

    Updates

---
+++ precache-manifest.e7a2fd2eb3e033e3ea208722f2c93fb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "65bce12f2ea6d16d601b",
+    "url": "/table/static/js/main.65bce12f.chunk.js"
+  },
+  {
+    "revision": "b6e9ce19e580d08fad4b",
+    "url": "/table/static/js/runtime~main.b6e9ce19.js"
+  },
+  {
+    "revision": "d146c6b91c5ebcdcc427",
+    "url": "/table/static/css/2.b3065bee.chunk.css"
+  },
+  {
+    "revision": "d146c6b91c5ebcdcc427",
+    "url": "/table/static/js/2.d146c6b9.chunk.js"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/table/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/table/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/table/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "6bc0538b1bfb0d8192bb566771e2a1e4",
+    "url": "/table/index.html"
+  }
+];
